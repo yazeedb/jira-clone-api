849227799087e771d2584ff2cfd2ce5816f68a1f
Yazeed Bzadough <ybzadough@gmail.com>
2020-03-06 20:20:30 -0500

    working sessionsc

--- server.js
+++ server.js
@@ -1,7 +1,75 @@
 const express = require('express');
-
-const app = express();
-const PORT = 8000;
+const sessions = require('client-sessions');
+const bodyParser = require('body-parser');
 const env = require('./env');
 
-app.post('/login', (req, res) => {});
+const app = express();
+const twoSeconds = 2000;
+const port = 8000;
+
+const db = [
+  {
+    username: 'yazeed',
+    password: '123'
+  },
+  {
+    username: 'bill',
+    password: '123'
+  }
+];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(
+  sessions({
+    cookieName: env.sessionName,
+    secret: env.secret,
+    duration: twoSeconds,
+    cookie: {
+      maxAge: twoSeconds,
+      httpOnly: true
+    }
+  })
+);
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const existingUser = db.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (!existingUser) {
+    return res.status(401).json({
+      message: 'Username/password combo not found!'
+    });
+  }
+
+  req[env.sessionName] = { username };
+
+  return res.json({
+    message: 'Success!'
+  });
+});
+
+app.get('/user', (req, res) => {
+  const { username } = req[env.sessionName];
+
+  console.log('username is', username);
+
+  if (!username) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  return res.json({
+    username,
+    message: 'Success!'
+  });
+});
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
